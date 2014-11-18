ea7dfc523ec538ca5663d0ee740c8b808dc57dc1
tkmagesh <tkmagesh77@gmail.com>
2014-11-18 10:54:53 +0530

    Web Workers Demo

---
+++ Day-02/02-WebWorkers/workerScript.js
@@ -0,0 +1,25 @@
+function doWork(){
+    for(var i=0;i<10000;i++){
+        for(var j=0;j<10000;j++)
+            for(var k=0;k<100;k++)
+            {}
+        if (i % 100 === 0){
+            var percentCompleted = i / 100;
+            self.postMessage({
+                type : "progress",
+                percentCompleted : percentCompleted + 1
+            });
+        }
+    }
+}
+self.addEventListener("message", function(msgEvtArg){
+    if (msgEvtArg.data === "start"){
+        doWork();
+        self.postMessage({
+            type : "completion"
+        });
+    } else {
+        console.log("unknown message received from main ", msgEvtArg.data);
+    }
+});
+
